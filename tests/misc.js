fef4b38a2cb0e5badeb07b30f7e40124aace2fdb
Davi Souza <ddas.souza@gmail.com>
2020-09-27 23:51:33 -0300

    change docker compose

--- tests/misc.js
+++ tests/misc.js
@@ -1,3 +1,5 @@
 const {get_material_types} = require('../services/db/material');
+const {get_parts} = require('../services/db/part');
 
-get_material_types().then(console.log);
+//get_material_types().then(console.log);
+get_parts().then(x => console.log(JSON.stringify(x))).catch(console.error);
